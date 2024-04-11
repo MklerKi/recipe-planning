1403b54e37d2f933a12f21b30f611a6e41ae65f5
MklerKi <marykain1.com@gmail.com>
2024-04-11 20:56:42 +0200

    init commit

---
+++ src/main/java/com/project/recipeplanning/RecipePlanningApplication.java
@@ -0,0 +1,13 @@
+package com.project.recipeplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipePlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipePlanningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/recipeplanning/RecipePlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.recipeplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipePlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
